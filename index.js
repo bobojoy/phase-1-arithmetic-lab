f3dba06449383f844305c3ee5a6a4ee8a9d9ed26
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-06-26 23:45:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-
 // Write your code here
+//multiply
+const num1 = 31;
+const num2 =2;
+
+const multiply = Number(num1)*Number(num2);
+console.log(multiply);
+
+//random
+const random  = parseInt(Math.random(2, 1000)*100)
+console.log(random);
+
+//mod
+const num3 =28;
+const num4 =6;
+const mod =num3 % num4
+
+//max
+let setss =[0,20,10,12,19]
+let nums='';
+console.log(...setss);
+
+let max =Math.max(...setss)
+console.log(max);
